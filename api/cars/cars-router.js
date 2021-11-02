12a630ad2b8f3384a14a8401645bee2d59de8923
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-02 12:54:22 -0700

    wired cars router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,8 @@
-// DO YOUR MAGIC
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  console.log("Reached /api/cars!");
+  res.status(200).json({ message: "Reached /api/cars!" });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,9 @@
-const express = require("express")
+const express = require("express");
+const CarsRouter = require("./cars/cars-router");
 
-const server = express()
+const server = express();
 
-// DO YOUR MAGIC
+server.use(express.json());
+server.use("/api/cars", CarsRouter);
 
-module.exports = server
+module.exports = server;
