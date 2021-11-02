245042a9b20a28d8750a36ffe58c941eca9094a0
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-02 13:32:24 -0700

    created cars model and added 2 cars to database

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,22 @@
-const getAll = () => {
-  // DO YOUR MAGIC
-}
+const db = require("./../../data/db-config");
 
-const getById = () => {
-  // DO YOUR MAGIC
-}
+const getAll = async () => {
+  const result = await db("cars");
+  return result;
+};
 
-const create = () => {
-  // DO YOUR MAGIC
-}
+const getById = async (id) => {
+  const result = await db("cars").where({ id });
+  return result;
+};
+
+const create = async (car) => {
+  const result = await db("cars").insert(car);
+  return result;
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
